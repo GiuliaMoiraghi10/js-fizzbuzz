fbb4de10648f512ba4ea9b931a668d9a2051d192
GiuliaMoiraghi10 <gmoiraghi10@gmail.com>
2024-10-15 15:17:56 +0200

    add structure js

---
+++ js/script.js
@@ -0,0 +1,14 @@
+console.log ("FizzBuzz")
+
+//CONSEGNA
+// Scrivi un programma che stampi i numeri da 1 a 100,
+//ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz. Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+
+
+// 1. Scrivi i numeri da 1 a 100
+// 2. Multipli di 3
+//    - "Fizz" al posto del numero
+// 3. Multipli di 5
+//    - "Buzz" al posto del numero
+// 4. Multipli di 3 e di 5
+//    - "FizzBuzz" al posto del numero
